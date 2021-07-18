d62bd3861a7801f5c8f5d6a7df3e2ee68fc73eb2
Icseon <me@icseon.com>
2021-07-18 02:03:10 +0200

    fix log

--- relay_tcp_server/server.py
+++ relay_tcp_server/server.py
@@ -53,7 +53,7 @@ class RelayTCPClient:
                 print(e)
 
                 f = open('relay_exceptions', 'a')
-                f.write(e)
+                f.write(str(e))
                 f.close()
 
                 # If the client is in the server client container, remove it from the server client container
