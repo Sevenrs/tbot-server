26bfdd0fcc071f5d4e98417fc86a3d634cee34c5
Icseon <me@icseon.com>
2021-07-21 19:55:38 +0200

    Revert possibility to wear two mercs

--- GameServer/Controllers/Shop.py
+++ GameServer/Controllers/Shop.py
@@ -236,13 +236,6 @@ def wear_item(**_args):
 
     item = inventory[slot]
 
-    # If we have a wearing merc, change the item type to merc_2
-    for wearing_idx in wearing_items['items']:
-        if wearing_items['items'][wearing_idx]['type'] == 'merc1':
-            item['type'] = 'merc2'
-
-    print(item)
-
     # Check if we have anything wearing in the slot we are trying to overwrite. If we are, do not replace the inventory
     # slot with 0, but replace it with the item we are wearing
     wearing_item = 0
