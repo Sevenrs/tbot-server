ad346a239f2820bad06eab10aae5293cf051d961
Icseon <me@icseon.com>
2021-08-22 16:16:02 +0200

    Refactor of packet handler

---
+++ GameServer/Controllers/data/packets.py
@@ -0,0 +1,71 @@
+from GameServer.Controllers import BoutLogin, Lobby, Shop, Guild, Friend, Inbox, Room, Character, Game
+
+PACKET_NAME             = 0
+PACKET_HANDLER          = 1
+PACKET_MYSQL_REQUIRED   = 2
+
+PACKET_READ = {
+    '0200': ('PACKET_PONG',                     BoutLogin.pong,             False),
+    'f82a': ('PACKET_ID_REQUEST',               BoutLogin.id_request,       True),
+    'f92a': ('PACKET_GET_CHARACTER',            BoutLogin.get_character,    True),
+    'fa2a': ('PACKET_CREATE_CHARACTER',         BoutLogin.create_character, True),
+    '222b': ('PACKET_EXIT_SERVER',              BoutLogin.exit_server,      False),
+
+    '082b': ('PACKET_LOBBY_REQUEST',            Lobby.GetLobby,             True),
+    '1a27': ('PACKET_LOBBY_CHAT',               Lobby.Chat,                 True),
+    '412b': ('PACKET_LOBBY_EXAMINE_PLAYER',     Lobby.examine_player,       True),
+    '442b': ('PACKET_LOBBY_WHISPER',            Lobby.Whisper,              False),
+    '0a2b': ('PACKET_LOBBY_ROOMS',              Lobby.RoomList,             False),
+
+    '272b': ('PACKET_FRIEND_REQUEST',           Friend.FriendRequest,       False),
+    '242b': ('PACKET_FRIEND_REQUEST_RESULT',    Friend.FriendRequestResult, True),
+    '252b': ('PACKET_FRIEND_DELETE',            Friend.DeleteFriend,        True),
+
+    '282b': ('PACKET_INBOX_REQUEST',            Inbox.RequestInbox,         True),
+    '292b': ('PACKET_INBOX_SEND',               Inbox.SendMessage,          True),
+    '2a2b': ('PACKET_INBOX_VIEW',               Inbox.RequestMessage,       True),
+    '2b2b': ('PACKET_INBOX_DELETE',             Inbox.DeleteMessage,        True),
+
+    '512b': ('PACKET_SHOP_SYNC_CASH',           Shop.sync_cash_rpc,         True),
+    '022b': ('PACKET_SHOP_PURCHASE_GOLD',       Shop.purchase_item,         True),
+    '042b': ('PACKET_SHOP_PURCHASE_CASH',       Shop.purchase_item,         True),
+    '032b': ('PACKET_SHOP_SELL',                Shop.sell_item,             True),
+    'fc2a': ('PACKET_SHOP_EQUIP_PART',          Shop.wear_item,             True),
+    '322b': ('PACKET_SHOP_EQUIP_ACCESSORY',     Shop.wear_item,             True),
+    '342b': ('PACKET_SHOP_EQUIP_PACK',          Shop.wear_item,             True),
+    'fd2a': ('PACKET_SHOP_REMOVE_PART',         Shop.unwear_item,           True),
+    '332b': ('PACKET_SHOP_REMOVE_ACCESSORY',    Shop.unwear_item,           True),
+    '352b': ('PACKET_SHOP_REMOVE_PACK',         Shop.unwear_item,           True),
+
+    '552b': ('PACKET_GUILD_CREATE',             Guild.Create,                   True),
+    '562b': ('PACKET_GUILD_APPLY',              Guild.SendGuildApplication,     True),
+    '572b': ('PACKET_GUILD_CANCEL_APPLICATION', Guild.CancelGuildApplication,   True),
+    '642b': ('PACKET_GUILD_GET_APPLICATIONS',   Guild.FetchGuildApplications,   True),
+    '5a2b': ('PACKET_GUILD_ACCEPT_APPLICATION', Guild.AcceptApplication,        True),
+    '5b2b': ('PACKET_GUILD_REFUSE_APPLICATION', Guild.RejectApplication,        True),
+    '5d2b': ('PACKET_GUILD_LEAVE',              Guild.LeaveGuild,               True),
+    '592b': ('PACKET_GUILD_EXPEL_MEMBER',       Guild.ExpelGuildMember,         True),
+    '732b': ('PACKET_GUILD_UPDATE_NOTICE',      Guild.UpdateGuildNotice,        True),
+
+    '062b': ('PACKET_ROOM_JOIN',                Room.join_room,             True),
+    '092b': ('PACKET_ROOM_CREATE',              Room.create,                False),
+    '0e2b': ('PACKET_ROOM_QUICK_JOIN',          Room.quick_join,            False),
+    '0b2b': ('PACKET_ROOM_START_GAME',          Room.start_game,            False),
+    '392b': ('PACKET_ROOM_UPDATE_SLOT_STATUS',  Room.set_status,            False),
+    '402b': ('PACKET_ROOM_KICK_PLAYER',         Room.kick_player,           False),
+    '422b': ('PACKET_ROOM_EXIT',                Room.exit_room,             False),
+    '652b': ('PACKET_ROOM_SET_LEVEL',           Room.set_level,             False),
+    '7a2b': ('PACKET_ROOM_SET_DIFFICULTY',      Room.set_difficulty,        False),
+    '782b': ('PACKET_ROOM_ENTER_SHOP',          Room.enter_shop,            False),
+    '792b': ('PACKET_ROOM_EXIT_SHOP',           Room.exit_shop,             True),
+
+    '6f2b': ('PACKET_GAME_PLAYER_DEATH',        Game.player_death_rpc,      False),
+    '362b': ('PACKET_GAME_USE_FIELD_PACK',      Game.use_field_pack,        True),
+    '3a2b': ('PACKET_GAME_MONSTER_DEATH',       Game.monster_kill,          False),
+    '3c2b': ('PACKET_GAME_USE_ITEM',            Game.use_item,              True),
+    '3e2b': ('PACKET_GAME_LOAD_FINISH',         Game.load_finish,           False),
+    '3b2b': ('PACKET_GAME_LOSE',                Game.game_end_rpc,          True),
+    '462b': ('PACKET_GAME_WIN',                 Game.game_end_rpc,          True),
+    'a627': ('PACKET_GAME_CHAT_COMMAND',        Game.chat_command,          False),
+    'a628': ('PACKET_GAME_SET_ATTACK_SCORE',    Game.set_score,             False)
+}
--- GameServer/Router.py
+++ GameServer/Router.py
@@ -7,7 +7,7 @@ __version__ = "1.0"
 import MySQL.Interface as MySQL
 
 # Import all packet handlers to use throughout the service
-from GameServer.Controllers import BoutLogin, Lobby, Shop, Guild, Friend, Inbox, Room, Character, Game
+from GameServer.Controllers.data.packets import PACKET_READ, PACKET_NAME, PACKET_HANDLER, PACKET_MYSQL_REQUIRED
 
 """
 This method will link the incoming packet to the right controller
@@ -27,91 +27,31 @@ def route(socket, packet, server, client, connection_handler):
             packet.id == 'fa2a'):
         raise Exception('Invalid packet received')
 
-    # Create new MySQL connection
-    mysql_connection = MySQL.GetConnection()
-    mysql = mysql_connection.cursor(dictionary=True, buffered=True)
+    # Default the MySQL connection and cursor to NoneTypes
+    mysql_connection, mysql = None, None
 
-    # Define packet commands to handle as well as their methods
-    packets = {
+    # If the packet is in the PACKET_READ object, handle it
+    if packet.id in PACKET_READ:
 
-        # Controller: Bout Authentication
-        '0200': BoutLogin.pong,
-        'f82a': BoutLogin.id_request,
-        'f92a': BoutLogin.get_character,
-        'fa2a': BoutLogin.create_character,
-        '222b': BoutLogin.exit_server,
+        received_packet = PACKET_READ[packet.id]
+        print("[GameServer] Parsing packet :: {0}".format(received_packet[PACKET_NAME]))
 
-        # Controller: Lobby
-        '082b': Lobby.GetLobby,
-        '1a27': Lobby.Chat,
-        '412b': Lobby.examine_player,
-        '442b': Lobby.Whisper,
-        '0a2b': Lobby.RoomList,
+        # If we need a MySQL connection, create one
+        if received_packet[PACKET_MYSQL_REQUIRED]:
+            mysql_connection = MySQL.GetConnection()
+            mysql = mysql_connection.cursor(dictionary=True, buffered=True)
 
-        # Controller: Friends
-        '272b': Friend.FriendRequest,
-        '242b': Friend.FriendRequestResult,
-        '252b': Friend.DeleteFriend,
+        # Handle the packet
+        received_packet[PACKET_HANDLER](**locals())
 
-        # Controller: Inbox
-        '282b': Inbox.RequestInbox,
-        '292b': Inbox.SendMessage,
-        '2a2b': Inbox.RequestMessage,
-        '2b2b': Inbox.DeleteMessage,
+    # If we could not find the packet, handle it otherwise
+    else:
+        unknown(**locals())
 
-        # Controller: Shop
-        '512b': Shop.sync_cash_rpc,
-        '022b': Shop.purchase_item,     # Gold items
-        '042b': Shop.purchase_item,     # Cash items
-        '032b': Shop.sell_item,
-
-        'fc2a': Shop.wear_item,         # Parts
-        '322b': Shop.wear_item,         # Accessories
-        '342b': Shop.wear_item,         # Packs
-
-        'fd2a': Shop.unwear_item,       # Parts
-        '332b': Shop.unwear_item,       # Accessories
-        '352b': Shop.unwear_item,       # Packs
-
-        # Controller: Guild
-        '552b': Guild.Create,
-        '562b': Guild.SendGuildApplication,
-        '572b': Guild.CancelGuildApplication,
-        '642b': Guild.FetchGuildApplications,
-        '5a2b': Guild.AcceptApplication,
-        '5b2b': Guild.RejectApplication,
-        '5d2b': Guild.LeaveGuild,
-        '592b': Guild.ExpelGuildMember,
-        '732b': Guild.UpdateGuildNotice,
-
-        # Controller: Room
-        '062b': Room.join_room,
-        '092b': Room.create,
-        '0e2b': Room.quick_join,
-        '0b2b': Room.start_game,
-        '392b': Room.set_status,
-        '402b': Room.kick_player,
-        '422b': Room.exit_room,
-        '652b': Room.set_level,
-        '7a2b': Room.set_difficulty,
-        '782b': Room.enter_shop,
-        '792b': Room.exit_shop,
-
-        # Controller: Game
-        '6f2b': Game.player_death_rpc,
-        '362b': Game.use_field_pack,
-        '3a2b': Game.monster_kill,
-        '3c2b': Game.use_item,
-        '3e2b': Game.load_finish,
-        '3b2b': Game.game_end_rpc,  # Game lost
-        '462b': Game.game_end_rpc,  # Game won
-        'a627': Game.chat_command,
-        'a628': Game.set_score
-
-    }.get(packet.id, unknown)(**locals())
-
-    # Close MySQL connection after packet has been parsed
-    mysql_connection.close()
+    # If the mysql_connection is not None, close the cursor and connection
+    if mysql_connection is not None:
+        mysql.close()
+        mysql_connection.close()
 
 
 """
@@ -119,4 +59,4 @@ Handle unknown packets
 """
 
 def unknown(**_args):
-    print('Unknown packet: <0x{0}[len={2}]::{1}>'.format(_args['packet'].id, _args['packet'].data, len(_args['packet'].data)))
+    print('[GameServer] Unknown packet: <0x{0}[len={2}]::{1}>'.format(_args['packet'].id, _args['packet'].data, len(_args['packet'].data)))
