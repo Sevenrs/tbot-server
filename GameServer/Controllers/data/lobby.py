d09c2e4419e6aef0511e0f2b324469f4c1f4f4f9
Xander Sterrenburg <me@icseon.com>
2022-02-18 01:55:14 +0100

    Grammar

--- GameServer/Controllers/data/lobby.py
+++ GameServer/Controllers/data/lobby.py
@@ -1,1 +1,1 @@
-LOBBY_MSG = 'Welcome to T-Bot Rewritten! Join our Discord at https://discord.gg/Fa2zgNtegV to stay up to informed on the latest news', 1
+LOBBY_MSG = 'Welcome to T-Bot Rewritten! Join our Discord at https://discord.gg/Fa2zgNtegV to stay up date and informed with the latest news', 1
