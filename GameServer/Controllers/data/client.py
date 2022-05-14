3f56f37f0c84a5fae4499c2b99a577b5bbe38228
Xander Sterrenburg <me@icseon.com>
2022-05-14 03:19:05 +0200

    Push client version

--- GameServer/Controllers/data/client.py
+++ GameServer/Controllers/data/client.py
@@ -1,8 +1,8 @@
-CLIENT_VERSION  = '1.0.3.1'     # Client version
+CLIENT_VERSION  = '1.0.3.2'     # Client version
 PING_TIMEOUT    = 90            # Connection timeout in seconds
 
 CLIENT_FILE_HASHES = [
-    'f8bef447dfc6653b26432ed9f730c28b', # TbotShield.dll
+    'b82b7e81fdef722f85336def186b0430', # TbotShield.dll
     'b6a4934b9d8f2ed02b5fd796b1bfd6ee', # script/dungeon/dungeon.bin
     '6fbada1f64ceb6db89f97fffea427103', # script/dungeon/p_monai.bin
     '32de15763d99e8ee86a24642d84de501', # script/dungeon/p_monimg.bin
@@ -12,9 +12,9 @@ CLIENT_FILE_HASHES = [
     'e92b2c244d5c3c52b59c1199bacb99fa', # script/env_cloc.sys
     'cb530999438604690c39e9015496070e', # script/file.sds
     '8f47226333f09c85e6680fd2f17ab94f', # script/ftinfo.sys
-    'dd8707c5b6d2598f87d355511a8d2c5e', # script/item.bin
+    'd6cd3e4f44d9070c9967cff3d9e90dde', # script/item.bin
     'aa7da1bd9309f422f0b1fd9725dfa28d', # script/mercinfo.bin
-    'df4c9044e38d0e0d946d8b0986d68d19', # script/shop.bin
+    'cea06dd6c2d06f10acf0b7f31e8fd59c', # script/shop.bin
     '443897a2bc6a8d0a0f208e0893dbbd1f', # script/skillpk.bin
     'b19d22b003ca6c9928799e81ba01efa1', # script/slangs.bin
     'ba093d81860a972acba81a488715c1bd', # script/string.bin
