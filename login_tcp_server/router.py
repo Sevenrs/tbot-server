39a6a06ac4458bef577e5f008f34d4620d256735
Xander Sterrenburg <me@icseon.com>
2023-01-24 15:09:03 +0100

    Update Login server to authenticate locally

--- login_tcp_server/router.py
+++ login_tcp_server/router.py
@@ -12,13 +12,73 @@ import MySQL.Interface as MySQL
 from login_tcp_server.data.authenticate import *
 from ratelimit import LOGIN_RATE_LIMIT
 from dotenv import dotenv_values
-import requests
+from pyrate_limiter import BucketFullException
+import argon2
 
 
 def route(client, packet):
     packets = {
-        'f82a': authenticate
+        'f82a': login
     }.get(packet.id, unknown)(**locals())
+
+
+'''
+This method will validate the username and password and will return a status
+'''
+
+
+def authenticate(username, password, ip_address, mysql_cursor):
+
+    # Attempt to find the user in the database
+    mysql_cursor.execute('''SELECT * FROM `users` WHERE `username` = %s''', [username])
+    user = mysql_cursor.fetchone()
+
+    # Initialize a default state object
+    state = {
+        "status": None,
+        "warnet": False
+    }
+
+    try:
+
+        # If the user couldn't be found, we're going to return RESPONSE_USER_NOT_FOUND
+        if user is None:
+            state['status'] = RESPONSE_USER_NOT_FOUND
+            return state
+
+        # Verify user password
+        argon2.PasswordHasher().verify(user['password'], password)
+
+        # If the user hasn't verified their email yet, we'll return RESPONSE_EMAIL_UNVERIFIED
+        if user['email_verified'] == 0:
+            state['status'] = RESPONSE_EMAIL_UNVERIFIED
+            return state
+
+        # If the user has been suspended, we'll return RESPONSE_USER_SUSPENDED
+        if user['suspended'] == 1:
+            state['status'] = RESPONSE_USER_SUSPENDED
+            return state
+
+        # If we're still there, we've successfully authenticated. Update the last IP address
+        mysql_cursor.execute('''UPDATE `users` SET `last_ip` = %s WHERE `username` = %s''', [
+            ip_address,
+            username
+        ])
+
+        # Update state accordingly and return the state
+        state['status'] = RESPONSE_SUCCESS
+        state['warnet'] = user['warnet_bonus'] == 1
+
+    # Handle incorrect passwords
+    except argon2.exceptions.VerifyMismatchError:
+        LOGIN_RATE_LIMIT.try_acquire('LOGIN_{0}'.format(ip_address))
+        state['status'] = RESPONSE_INCORRECT_PASSWORD
+
+    # Handle rate limit
+    except BucketFullException:
+        state['status'] = RESPONSE_RATE_LIMIT
+
+    return state
 
 
 '''
@@ -26,86 +86,36 @@ This method will give users the possibility to authenticate
 '''
 
 
-def authenticate(**_args):
-    # Read username and password from the packet
+def login(**_args):
+    # Read username and password from the packet. We'll be skipping one character (H) with the username.
     username = _args['packet'].read_string()[1:]
     password = _args['packet'].read_string()
+
+    # Create a new MySQL connection. We're going to require it to we can start finding the user of relevance.
+    connection = MySQL.get_connection()
+    cursor = connection.cursor(dictionary=True)
+
+    # Let's see what the authentication result is
+    authentication_result = authenticate(username=username,
+                                         password=password,
+                                         ip_address=_args['client']['address'][0],
+                                         mysql_cursor=cursor)
 
     # Construct result packet
     result = PacketWrite()
     result.add_header([0xEC, 0x2C])
     result.append_bytes([0x01, 0x00])
 
-    # Read environment variables
-    env = dotenv_values('.env')
+    # Add result status to the packet
+    result.append_integer(authentication_result['status'], 1, 'little')
 
-    try:
+    # Add warnet state to the packet
+    result.append_bytes([0x00, authentication_result['warnet']])
 
-        # Consume login rate limit point
-        LOGIN_RATE_LIMIT.try_acquire('LOGIN_{0}'.format(_args['client']['address'][0]))
+    # Add the packet footer - skip first two bytes as we've just written them
+    result.append_bytes(PACKET_FOOTER[2:])
 
-        # Send credentials to the internal web API
-        response = requests.post("{0}/login".format(env['INTERNAL_ROOT_URL']),
-                                 json={
-                                     "username": username,
-                                     "password": password,
-                                     "ip": _args['client']['address'][0]
-                                 },
-
-                                 headers={
-                                     "Content-Type": "application/json",
-                                     "Authorization": env['INTERNAL_ACCESS_TOKEN']
-                                 })
-
-        # If the status code is not 200 OK, there's an error. Send the right error depending on said status code.
-        if response.status_code != 200:
-
-            # Deserialize the response
-            response = response.json()
-
-            # Check if the response contains errors. If not, throw an exception
-            if len(response['errors']) <= 0:
-                raise Exception('Invalid response from API')
-
-            # Read last error from response, retrieve the status code from the status map and send result to the client
-            error = response['errors'][0]['msg']
-            status = STATUS_MAP[error] if error in STATUS_MAP else RESPONSE_ERROR
-
-            # Append this status to the packet and send to the client
-            result.append_integer(status, 1, 'little')
-            result.append_bytes(PACKET_FOOTER)
-            _args['client']['socket'].sendall(result.packet)
-            return _args['client']['socket'].close()
-
-        # If we have successfully logged in, check if we have a user on record with the web ID provided by the service
-        web_id = response.json()['web_id']
-
-        # Create a new user if we have to
-        connection = MySQL.get_connection()
-        cursor = connection.cursor(dictionary=True)
-        cursor.execute(
-            '''INSERT INTO `users` (`external_id`) SELECT %s FROM DUAL WHERE NOT EXISTS (SELECT * FROM `users` WHERE 
-            `external_id`= %s LIMIT 1) ''',
-            [
-                web_id,
-                web_id
-            ])
-
-        # Close MySQL connection
-        connection.close()
-
-        result.append_integer(RESPONSE_SUCCESS, 1, 'little')  # Add success status to the packet and send to the client
-
-        # Append war-net bonus status to the packet. It's 1 if there's a bonus,
-        result.append_bytes([0x00, (0x01 if response.json()['warnet_bonus'] else 0x00)])
-        result.append_bytes(PACKET_FOOTER[2:])  # Skip the first two bytes because we've manually written those above
-
-    except Exception:
-
-        # In case of an error, send the RESPONSE_ERROR status to the client
-        result.append_integer(RESPONSE_ERROR, 1, 'little')
-        result.append_bytes(PACKET_FOOTER)
-
+    # Send the result packet and close the connection
     _args['client']['socket'].sendall(result.packet)
     return _args['client']['socket'].close()
 
--- ratelimit.py
+++ ratelimit.py
@@ -1,4 +1,4 @@
 from pyrate_limiter import Duration, RequestRate, Limiter
 
-LOGIN_RATE_LIMIT = Limiter(RequestRate(3, Duration.SECOND))
+LOGIN_RATE_LIMIT = Limiter(RequestRate(3, Duration.MINUTE))
 CHAT_RATE_LIMIT = Limiter(RequestRate(1, Duration.SECOND))
