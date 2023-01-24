f13c781b784e29430d798e03fd8a9e437a81f5ca
Xander Sterrenburg <me@icseon.com>
2023-01-24 15:46:53 +0100

    Small oopsie

--- GameServer/Controllers/handlers/moderation.py
+++ GameServer/Controllers/handlers/moderation.py
@@ -9,7 +9,7 @@ This method will suspend a player from the game
 
 
 def suspend_player(_args, user_id, connection_handler=None, client=None):
-    print('[moderation@suspend_player()] :: suspending player with user id: {0}'.format(web_id))
+    print('[moderation@suspend_player()] :: suspending player with user id: {0}'.format(user_id))
 
     # Suspend the user
     _args['mysql'].execute('''UPDATE `users` SET `suspended` = 1 WHERE `id` = ?''', [
